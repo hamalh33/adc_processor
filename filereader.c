3eadda56b0123a963b50ea04d521f2cb91abcb8d
h4med <hammed1122.az@gmail.com>
2026-06-29 17:51:11 +0100

    Load all records into memory and print the first sample

--- filereader.c
+++ filereader.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "adc.h"
+#include <stdlib.h>
+
 
 int main(int argc, char *argv[]) {
    if (argc != 2) {
@@ -29,7 +31,17 @@ int main(int argc, char *argv[]) {
     printf("Channels: %u\n", header.channel_count);
     printf("Records: %u\n", header.record_count);
 
+ADCSample *samples = malloc(header.record_count * sizeof(ADCSample));
+    if (samples == NULL) {
+        printf("Error allocating samples\n");
+        fclose(file);
+        return 1;
+    }
 
+    fread(samples,sizeof(ADCSample),header.record_count,file);
+    printf("first sample: channel=%u raw=%u\n", samples[0].channel_id, samples[0].raw_value);
+
+    free(samples);
     fclose(file);
     return 0;
 
